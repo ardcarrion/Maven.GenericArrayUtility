87c93b0bfecb76c88546c802c16413358679edbe
Alicia Carrion <alicia.r.carrion@gmail.com>
2019-06-24 13:56:15 -0400

    renamed field

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,22 +1,18 @@
 package com.zipcodewilmington.arrayutility;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.Stream;
 
 /**
  * Created by leon on 3/6/18.
  */
 public class ArrayUtility<T> {
 
-    private ArrayList<T> inputArray;
+    private ArrayList<T> inputArrayList;
     private T[] originalInput;
 
     public ArrayUtility(T[] input) {
-        inputArray = new ArrayList(Arrays.asList(input));
+        inputArrayList = new ArrayList(Arrays.asList(input));
         originalInput = input;
     }
 
@@ -45,19 +41,19 @@ public class ArrayUtility<T> {
 
     public Integer getNumberOfOccurrences(T valueToEvaluate) {
         Integer occurrences = 0;
-        for (T t : inputArray) if (valueToEvaluate.equals(t)) occurrences++;
+        for (T t : inputArrayList) if (valueToEvaluate.equals(t)) occurrences++;
         return occurrences;
     }
 
     public T[] removeValue(T valueToRemove)  {
-        while (inputArray.contains(valueToRemove)) inputArray.remove(valueToRemove);
-        return inputArray.toArray(Arrays.copyOf(originalInput, inputArray.size()));
+        while (inputArrayList.contains(valueToRemove)) inputArrayList.remove(valueToRemove);
+        return inputArrayList.toArray(Arrays.copyOf(originalInput, inputArrayList.size()));
     }
     public ArrayList<T> mergeArrays(T[] arrayToMerge) {
-        if (inputArray == null) return null;
-        if (arrayToMerge == null | arrayToMerge.length == 0) return inputArray;
-        ArrayList<T> merged = new ArrayList<>(inputArray.size() + arrayToMerge.length);
-        merged.addAll(inputArray);
+        if (inputArrayList == null) return null;
+        if (arrayToMerge == null | arrayToMerge.length == 0) return inputArrayList;
+        ArrayList<T> merged = new ArrayList<>(inputArrayList.size() + arrayToMerge.length);
+        merged.addAll(inputArrayList);
         merged.addAll(Arrays.asList(arrayToMerge));
         return merged;
     }
